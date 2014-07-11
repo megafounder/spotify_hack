02840437fa7d71e3da77dbba02e70dc39f351a0a
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-07-11 16:09:39 +0200

    Added play song

--- js/index.js
+++ js/index.js
@@ -1,53 +1,50 @@
-(function (global) {
+(function(global) {
   'use strict';
 
-var response;
+  var response;
 
-// get song from spotify's API
+  // get song from spotify's API
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.spotify.com/v1/tracks/2Foc5Q5nqNiosCNqttzHof');
 
-	var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.spotify.com/v1/tracks/7Bxv0WL7UC6WwQpk9TzdMJ');
+  xhr.setRequestHeader('Accept', 'application/json');
 
-    xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onload = function() {
+    if (this.status === 200) { // the result is OK
+      response = JSON.parse(this.response);
+      console.log('onload response', response);
+      setingData(response);
+      playSong(response);
+    }
+  };
 
-    xhr.onload = function () {
-      if (this.status === 200) { // the result is OK
-        response = JSON.parse(this.response);
-        console.log('onload response', response);
-        handleResponse(response);
-        playSong(response);
-      }
-    };
+  xhr.send();
 
-    xhr.send();
+  var setingData = function(response) {
+    // set up the data of the player
+    var albumName = response.album.name;
+    var songName = response.name;
+    var authorName = response.artists[0].name;
 
-// show song
+    $('.title').text(songName);
+    $('.author').text(authorName);
+    $('.btn-play').removeClass('disabled');
+  };
 
-	var handleResponse = function(response) {
+  // play song
+  var playSong = function(response) {
+    var songUrl = response.preview_url;
+    $('#audio').attr('src', songUrl);
 
-		var albumName = response.album.name;
-		var songName = response.name;
-		var authorName = response.artists[0].name;
+    $('#playbutton').on('click', function() {
+      $(this).toggleClass('playing');
 
+      if ($(this).hasClass('playing'))
+        document.getElementById('audio').play();
+      else
+        document.getElementById('audio').pause();
+    });
 
-		$('.title').text(songName);
-		$('.author').text(authorName);
-		$('.btn-play').removeClass('disabled');
-
-
-	};
-	
-// play song
-
-	var playSong = function(response){
-
-	var songUrl = response.external_urls.preview_url;
-	$('#audio').attr('src', 'http://troll.com');
-	$('#playbutton').on('click', function(){
-		document.getElementById('audio').play();
-		
-	});
-
-	};
+  };
 
 })(window);
