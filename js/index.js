3746a586e1ca271a8a07ed5b6b38c5a19e779315
Jonathan Garcia <ceo@megafounder.com>
2014-07-11 15:43:38 +0200

    added new functions troll

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 var response;
 
 // get song from spotify's API
+
 	var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://api.spotify.com/v1/tracks/7Bxv0WL7UC6WwQpk9TzdMJ');
 
@@ -14,6 +15,7 @@ var response;
         response = JSON.parse(this.response);
         console.log('onload response', response);
         handleResponse(response);
+        playSong(response);
       }
     };
 
@@ -30,9 +32,22 @@ var response;
 
 		$('.title').text(songName);
 		$('.author').text(authorName);
+		$('.btn-play').removeClass('disabled');
 
 
 	};
 	
+// play song
+
+	var playSong = function(response){
+
+	var songUrl = response.external_urls.preview_url;
+	$('#audio').attr('src', 'http://troll.com');
+	$('#playbutton').on('click', function(){
+		document.getElementById('audio').play();
+		
+	});
+
+	};
 
 })(window);
